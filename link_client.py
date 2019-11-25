d7fa7be4941e585e888df8444099e2d37fbf0797
todownload <49515565+todownload@users.noreply.github.com>
2019-11-25 20:55:11 +0800

    1

---
+++ link_client.py
@@ -0,0 +1,31 @@
+"""
+发送文件给服务器
+"""
+
+import socket
+from time import sleep
+
+# 用于找到文件位置
+path = ""
+fileName = "desktop.bmp"
+
+# host = '192.168.174.133'
+host = "192.168.43.240"
+port = 9678
+
+def main():
+    client = socket.socket()
+    client.connect((host,port))
+    client.send(fileName.encode(encoding="utf-8")) # 发送文件名
+    sleep(2) # 等待2秒
+    is_ac = client.recv().decode(encoding="utf-8") # 是否ack
+    if is_ac == "ACK":  # 发送数据
+        fp = path+fileName
+        with open(fp,"rb") as fr:
+            data = fr.read()
+            client.send(data)
+    pass
+
+
+if __name__ == "__main__":
+    main()
